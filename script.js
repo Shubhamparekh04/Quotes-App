6d86043d44a1d82e2db5d51d1a598b8ad84b8b09
Shubhamparekh04 <shubhamparekh04@gmail.com>
2024-11-20 19:46:15 +0530

    eliminates the explicit if-else structure

--- script.js
+++ script.js
@@ -198,39 +198,21 @@ let Data = [
 let rec = document.querySelector('.marketing');
 
 Data.forEach((element, index) => {
-    if (index % 2 != 0) {
-        rec.innerHTML += `
+    rec.innerHTML += `
         <hr class="featurette-divider">
         <div class="row featurette">
-            <div class="col-md-7 test">
+            <div class="col-md-7 ${(index % 2 == 0) ? "order-md-2 test" : "test"}">
                 <h2 class="featurette-heading fw-normal lh-1">${element.quote}
                     <h3 class="text-body-secondary">-${element.author}</h3>
                 </h2>
                 <p class="lead">${element.birthPlace}</p>
             </div>
-            <div class="col-md-5 d-flex justify-content-center">
+            <div class="col-md-5 ${(index % 2 == 0) ? "order-md-1 d-flex justify-content-center" : "d-flex justify-content-center"}">
                 <img src="${element.imgurl}" class="rounded-image" alt="${element.author}">
             </div>
         </div>
         <hr class="featurette-divider">
         `;
-    } else {
-        rec.innerHTML += `
-        <hr class="featurette-divider">
-        <div class="row featurette">
-            <div class="col-md-7 order-md-2 test">
-                <h2 class="featurette-heading fw-normal lh-1">${element.quote}
-                    <h3 class="text-body-secondary">-${element.author}</h3>
-                </h2>
-                <p class="lead">${element.birthPlace}</p>
-            </div>
-            <div class="col-md-5 order-md-1 d-flex justify-content-center">
-                <img src="${element.imgurl}" class="rounded-image" alt="${element.author}">
-            </div>
-        </div>
-        <hr class="featurette-divider">
-        `;
-    }
 });
 
 
